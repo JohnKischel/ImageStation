3e45a67361798be6102113d4afdf2c6eab9190dd
johnkischel <johnkischel@gmail.com>
2021-05-02 23:15:37 +0100

    made changes in rules and updated installimage.sh

--- scripts/installimage.sh
+++ scripts/installimage.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
+
+get_logtime(){
+	LOGTIME=$(date +%d.%h_%H:%M:%S)
+	echo $LOGTIME
+}
+
 DEVPATH="$1"
 SCRIPTROOT=$(dirname $0)
-IMAGEPATH=$(realpath $SCRIPTROOT/../currentiso/*.iso)
 LOGPATH=$(realpath $SCRIPTROOT/../log/)
+IMAGEPATH=$(realpath $SCRIPTROOT/../currentiso/*.iso)
+echo "$(get_logtime)> Script was triggered by udev and started." >> $LOGPATH/install.log
+echo "$(get_logtime)> DEVPATH=$DEVPATH" >> $LOGPATH/install.log 
 
-sudo dd if="/dev/zero" of=$DEVPATH count=1 bs=512 && echo "Deleted MBR of $DEVPATH" >> $LOGPATH/install.log
 
-sudo dd if=$IMAGEPATH of=$DEVPATH bs=1M & 
-export APP_PID=$!
-if [ ! -z "$APP_PID" ]
+if [ -e $IMAGEPATH ]
 then
-	echo "Starting installation to $DEVPATH with image $IMAGEPATH" >> $LOGPATH/install.log
-	echo "Process id of dd: $APP_PID" >> $LOGPATH/install.log
-	while [ -e "/proc/$APP_PID" ]
-	do
-		LOGTIME=$(date +%d.%h_%H:%M:%S)
-		$(echo "$LOGTIME > Process id: $APP_PID" >> $LOGPATH/install.log)
-		sleep 5s
-		if [ ! -e /proc/$APP_ID ]
-		then
-			$(echo "----- Script finished -----" >> $LOGPATH/install.log)
-		fi
-	done
-fi	
-$(echo "----- Script finished -----" >> $LOGPATH/install.log)
+	# Delete the mbr.
+	dd if="/dev/zero" of=$DEVPATH count=1 bs=512 && echo "$(get_logtime)> Deleted MBR of $DEVPATH" >> $LOGPATH/install.log && sudo dd if=$IMAGEPATH of=$DEVPATH bs=1M & 
+
+	# Start the copy job and export the process id for later usage.
+	export APP_PID=$!
+	if [ ! -z "$APP_PID" ]
+	then
+		echo "$(get_logtime)> Starting installation to $DEVPATH with image $IMAGEPATH" >> $LOGPATH/install.log
+		echo "$(get_logtime)> Process id of dd: $APP_PID" >> $LOGPATH/install.log
+		while [ -e "/proc/$APP_PID" ]
+		do
+			$(echo "$(get_logtime)> Process id: $APP_PID" >> $LOGPATH/install.log)
+			sleep 5s
+			if [ ! -e /proc/$APP_ID ]
+			then
+				$(echo "----- Script finished -----" >> $LOGPATH/install.log)
+			fi
+		done
+	fi	
+	$(echo "----- Script finished -----" >> $LOGPATH/install.log)
+	$(sync)
+else
+	$(echo "No iso detected exiting script." >> $LOGPATH/install.log)
+fi
+
